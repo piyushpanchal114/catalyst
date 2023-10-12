5fee9742786842c17ecb564c6ff3b0d730909506
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-10-12 22:35:59 +0530

    add installed apps

--- netly/netly/settings.py
+++ netly/netly/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "rest_framework",
+
     "core"
 ]
 
@@ -119,6 +121,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = "core.User"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
