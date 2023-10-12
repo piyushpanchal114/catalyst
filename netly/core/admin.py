6d7cfa849f24b2785d2703d5e8b30e712d2824aa
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-10-12 22:35:31 +0530

    admin panel created

--- netly/core/admin.py
+++ netly/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import User, Genre, Movie
 
-# Register your models here.
+# Register your models
+
+admin.site.register(User)
+admin.site.register(Genre)
+admin.site.register(Movie)
