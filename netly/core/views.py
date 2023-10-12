f298d9921e0b81ae1e57116c75d1615f2ef4924e
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-10-12 22:35:20 +0530

    views created

--- netly/core/views.py
+++ netly/core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from rest_framework import viewsets
+from .models import Genre, Movie
+from .serializers import GenreSerializer, MovieSerializer, UpdateMovieSerializer
 
 
 def image_upload(request):
@@ -13,3 +16,21 @@ def image_upload(request):
             "image_url": image_url
         })
     return render(request, "upload.html")
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.select_related("genre").all()
+    # serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == "PUT":
+            return UpdateMovieSerializer
+        elif self.request.method == "POST":
+            return UpdateMovieSerializer
+        else:
+            return MovieSerializer
